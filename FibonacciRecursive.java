c865a9965655fbbd01aba3133cf3473948ced2c4
Mehmet Ali Cuhaci <mehmet.cuhaci10@gmail.com>
2023-04-03 18:51:51 +0300

    .

---
+++ FibonacciRecursive.java
@@ -0,0 +1,17 @@
+package Pratikler;
+
+public class FibonacciRecursive {
+    // BUNA YENİDEN BAK UNUTMA !!!
+    static int f(int n) {
+        if (n==1 || n==2) {
+            return 1;
+        }
+        return f(n-1)+f(n-2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(f(11));
+    }
+
+}
+
