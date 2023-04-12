4fcbc58d82f3bd0d844f5bb1054321f65cdd36e1
Mehmet Ali Cuhaci <mehmet.cuhaci10@gmail.com>
2023-04-12 18:55:36 +0300

    .

---
+++ DeseneGoreMetot.java
@@ -0,0 +1,32 @@
+package Pratikler;
+
+import java.util.Scanner;
+
+public class DeseneGoreMetot {
+    static void figure () {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Bir Sayi Giriniz :");
+        int n = inp.nextInt();
+
+        int ilkSayi = n;
+        System.out.print("Çıktı :" +n+" ");
+
+        for (int i=n; i>0;i-=5){
+            ilkSayi-=5;
+            System.out.print(ilkSayi+" ");
+        }
+
+        int sonSayi =ilkSayi;
+        for (int i=sonSayi;i<n;i+=5){
+            sonSayi+=5;
+            System.out.print(sonSayi+" ");
+
+        }
+
+    }
+    public static void main(String[] args) {
+
+        figure();
+    }
+}
+
